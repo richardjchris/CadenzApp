860ae4b6aa401bf35537c328dbebe81541f1e589
Richard Christiansen <12730988@student.uts.edu.au>
2020-04-20 23:44:41 +1000

    Initial commit

---
+++ CadenzApp/wwwroot/js/Layouts/Sidenav.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $('.sidenav').sidenav();
+
+    //$(".navbar-toggler").click(function () {
+    //    setTimeout
+    //        (
+    //            function () {
+    //                $("#table-wrapper").DataTable().columns.adjust();
+    //            }, 300
+    //        )
+    //    var IsLocked = $(".sidenav-main").hasClass("nav-lock");
+    //    SetSidenavState(IsLocked);
+    //});
+
+    //function SetSidenavState(state) {
+    //    $.ajax({
+    //        url: baseUrl + controller + "/SetSidenavLock",
+    //        data: "State=" + state,
+    //        contentType: "json",
+    //        cache: false
+    //    });
+    //};
+});
