8c856857127c16fb4be8cce4d2c4fe63d24351a2
Richard Christiansen <12730988@student.uts.edu.au>
2020-05-03 12:59:16 +1000

    Initial creation

---
+++ CadenzApp/BusinessLogic/StudentsBusinessLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web;
+using System.Data.SqlClient;
+using e_Tracker.Models;
+using e_Tracker.Models.EDMX;
+
+namespace CadenzApp.BusinessLogic
+{
+	public class StudentsBusinessLogic
+	{
+		public string AddStudent(string Student)
+		{
+			return "Success";
+		}
+	}
+}
--- CadenzApp/Controllers/StudentsController.cs
+++ CadenzApp/Controllers/StudentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CadenzApp.BusinessLogic;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,10 +11,16 @@ namespace CadenzApp.Controllers
 {
     public class StudentsController : Controller
     {
+        protected readonly StudentsBusinessLogic BsLogic = new StudentsBusinessLogic();
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult AddStudent(string Student)
+        {
+            return Json(BsLogic.AddStudent(Student), System.Web.Mvc.JsonRequestBehavior.AllowGet);
         }
     }
 }
