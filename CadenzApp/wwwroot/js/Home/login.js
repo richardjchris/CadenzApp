70ce3243ebea0d800baec6ff41572e2cc3b19dcf
Richard Christiansen <12730988@student.uts.edu.au>
2020-06-10 00:22:17 +1000

    Updated login view

---
+++ CadenzApp/wwwroot/js/Home/login.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    if ($(window).width() < 650) {
+        ToggleLoginSplash();
+    }
+});
+
+$(window).resize(function () {
+    ToggleLoginSplash();
+});
+
+function ToggleLoginSplash() {
+    if ($(window).width() < 651) {
+        $("#login-page .card-panel").addClass("vertical");
+        $("#login-page .card-panel").removeClass("horizontal");
+        $("#login-page .card-panel").css("padding-left", 0);
+    }
+    else {
+        $("#login-page .card-panel").removeClass("vertical");
+        $("#login-page .card-panel").addClass("horizontal");
+        $("#login-page .card-panel").css("padding-left" , "initial");
+    }
+}
